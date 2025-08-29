313652f80337f0c07a447c94d806793a1334f397
Leonardo Vilela <leonardo@gmail.com>
2025-08-29 02:02:46 -0300

    Feat: Add novos campos na tabela de cliente e Lead

--- src/main/resources/db/migration/V1__create_usuario.sql
+++ src/main/resources/db/migration/V1__create_usuario.sql
@@ -1,3 +1,3 @@
-INSERT INTO usuario (id, created_at, email, email_verificado, nome, senha_hash, status, telefone, updated_at)
+INSERT INTO public.usuario (id, created_at, email, email_verificado, nome, senha_hash, status, telefone, updated_at)
 VALUES(1,  now(), 'jubileu@gmail.com', false, 'Leonardo2', 'dGVzdGU=', 'ATIVO', '349918127415',  now());
 
